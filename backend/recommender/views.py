2c18318c92d76e3365a24a503f68f28c6d14d2c6
Shriyan Bachigari <shriyanbachigari@gmail.com>
2025-07-04 23:43:44 -0400

    finished polls

--- backend/recommender/views.py
+++ backend/recommender/views.py
@@ -40,4 +40,4 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+        return HttpResponseRedirect(reverse("recommender:results", args=(question.id,)))
